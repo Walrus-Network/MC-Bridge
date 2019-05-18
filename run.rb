3b95ea3381cc2f9a3892f5f3e56263638973ad9f
Austin Mayes <austin@avicus.net>
2019-05-18 02:20:40 -0500

    Fix data parse

--- run.rb
+++ run.rb
@@ -1,8 +1,7 @@
 # Require nessecary libraries for the server
 require_relative "server"
-require "json"
 
 # Load the local server
 puts "Data: #{ARGV[0]}"
-@server = LocalServer.new("/server", JSON.parse(ARGV[0]))
+@server = LocalServer.new("/server", eval(ARGV[0]))
 @server.load!
